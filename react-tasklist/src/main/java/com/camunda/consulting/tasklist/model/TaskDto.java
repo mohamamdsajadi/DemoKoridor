9b5c3e4c72fa1b08e19593d8bdc72869f9220348
mohammadsajadi <92863880+mohamamdsajadi@users.noreply.github.com>
2026-06-20 10:21:04 +0330

    Enhance process category management

--- react-tasklist/src/main/java/com/camunda/consulting/tasklist/model/TaskDto.java
+++ react-tasklist/src/main/java/com/camunda/consulting/tasklist/model/TaskDto.java
@@ -1,6 +1,5 @@
 package com.camunda.consulting.tasklist.model;
 
-import com.fasterxml.jackson.databind.JsonNode;
 import java.util.Map;
 
-public record TaskDto(String id,String name,Map<String,Object> schema, Map<String, Object> data, String formKey) {}
+public record TaskDto(String id, String name, Map<String, Object> schema, Map<String, Object> data, String formKey) {}
